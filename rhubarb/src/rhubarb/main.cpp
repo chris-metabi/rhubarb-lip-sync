20dea6a301aedcfe5b71b3357289492481977067
Chris Calef <chris.calef@gmail.com>
2023-07-20 11:46:24 -0700

    ...

--- rhubarb/src/rhubarb/main.cpp
+++ rhubarb/src/rhubarb/main.cpp
@@ -263,7 +263,7 @@ int main(int platformArgc, char* platformArgv[]) {
 			std::cerr << " ... Got audio clip... size=" <<  audioClip->size() <<  std::endl;
 
 			//const Recognizer& recognizer = *createRecognizer(recognizerType.getValue());
-			std::cerr << " ... recognizer? ... " <<  std::endl;
+			std::cerr << " ... recognizer? ... " << recognizerType.getValue() << std::endl;
 
 			//const BoundedTimeline<Phone> phones = recognizer.recognizePhones(*audioClip, boost::optional<string>(), 1);//, progressSink);
 			std::cerr << " ... Phones? ... range?" <<  std::endl;
